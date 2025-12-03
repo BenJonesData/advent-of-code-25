14c8211bf2ccd4322f3958f641242fff0cadfde9
Ben Jones <benjones1479@gmail.com>
2025-12-03 19:54:50 +0000

    simplify helper

--- helpers.py
+++ helpers.py
@@ -5,13 +5,7 @@ def create_test_function(test_input, expected_output):
     return test_solution
 
 
-def input_to_list_by_line(file_name):
+def input_to_str(file_name):
     with open(f"inputs/{file_name}") as f:
-        file_str = f.read()
-
-    file_list = file_str.split("\n")
-
-    if file_list[-1] == "":
-        file_list = file_list[:-1]
+        return f.read()
     
-    return file_list
